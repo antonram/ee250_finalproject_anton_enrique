abe3a9a3e7167f32ce73b8b36e46c595cea2798f
Anton RP <antonram@usc.edu>
2021-12-03 15:21:43 -0800

    try schtuff

--- rpi1_pub.py
+++ rpi1_pub.py
@@ -46,32 +46,33 @@ if __name__ == '__main__':
 
     # constantly get data and publish it
     while True:
-        sound_data = grovepi.analogRead(sound_sensor)
-        client.publish('rpi1/sound_sensor', sound_data)
-        
-        if counter == 50:
-		    lcd.setText('TAKE A BREAK')
-        	CACHE[0] = ' ' + affirmations_api.AFFIRMATIONS_APP['init']()
-	        counter = 0
-        lcd.setText_norefresh(CACHE[0][ind:ind+LCD_LINE_LEN])
+        try:
+            sound_data = grovepi.analogRead(sound_sensor)
+            client.publish('rpi1/sound_sensor', sound_data)
+            
+            if counter == 50:
+                lcd.setText('TAKE A BREAK')
+                CACHE[0] = ' ' + affirmations_api.AFFIRMATIONS_APP['init']()
+                counter = 0
+            lcd.setText_norefresh(CACHE[0][ind:ind+LCD_LINE_LEN])
 
-        # ADDED SCROLL
-	    if ind < len(CACHE[0]):
-	        ind += 1
-	    else:
-	        ind = 0
+            # ADDED SCROLL
+            if ind < len(CACHE[0]):
+                ind += 1
+            else:
+                ind = 0
 
-        # print(CACHE[0]) <-- for debugging
-	    counter += 1
-	    # changes backlight to given color from color list (7 items)
-	    lcd.setRGB(colors[counter%7][0],colors[counter%7][1],colors[counter%7][2])
-		time.sleep(.1)
+            # print(CACHE[0]) <-- for debugging
+            counter += 1
+            # changes backlight to given color from color list (7 items)
+            lcd.setRGB(colors[counter%7][0],colors[counter%7][1],colors[counter%7][2])
+            time.sleep(.1)
 
-    except IOError:
-	    print ("Error")
+        except IOError:
+            print ("Error")
 
-    except KeyboardInterrupt:
-	    # Gracefully shutdown on Ctrl-C
-	    lcd.setText('')
-	    lcd.setRGB(0, 0, 0)
-	    break
+        except KeyboardInterrupt:
+            # Gracefully shutdown on Ctrl-C
+            lcd.setText('')
+            lcd.setRGB(0, 0, 0)
+            break
