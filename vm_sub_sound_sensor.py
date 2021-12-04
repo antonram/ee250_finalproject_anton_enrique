7d5ce472bd4a3b1b8513df8735e1681dfb7cd67a
Anton RP <antonram@usc.edu>
2021-12-03 17:40:45 -0800

    final schtuff

--- vm_sub_sound_sensor.py
+++ vm_sub_sound_sensor.py
@@ -8,7 +8,7 @@ import paho.mqtt.client as mqtt
 import time
 
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+# import matplotlib.animation as animation
 
 
 rpi1_values = []
@@ -29,27 +29,29 @@ def on_message(client, userdata, msg):
 
 def rpi1_sound_callback(client, userdata, msg):
     print('RPi 1 Sound: ' + str(msg.payload, 'utf-8'))
-    rpi1_values.append(msg.payload)
+    if len(rpi1_values) < 5:
+        rpi1_values.append(int(msg.payload))
 
 def rpi2_sound_callback(client, userdata, msg):
     print('RPi 2 Sound: ' + str(msg.payload, 'utf-8'))
-    rpi2_values.append(msg.payload)
+    if len(rpi2_values) < 5:
+        rpi2_values.append(int(msg.payload))
 
 def remove_max(values):
     max = -1
     for i in range(len(values)):
-        if int(values[i]) > max:
+        if int(values[i]) > int(max):
             max = values[i]
     values.remove(max)
 
 def remove_min(values):
     min = 10000
     for i in range(len(values)):
-        if int(values[i]) < min:
+        if int(values[i]) < int(min):
             min = values[i]
     values.remove(min)
 
-
+'''
 def animate(i, x, y, x_val, y_val, winner):
     x_val[-1] += 1
     y_val[-1] += winner
@@ -64,7 +66,7 @@ def animate(i, x, y, x_val, y_val, winner):
     plt.subplots_adjust(bottom=0.30)
     plt.title('Sexiness')
     plt.ylabel('Intensity')
-
+'''
 
 if __name__ == '__main__':
     #this section is covered in publisher_and_subscriber_example.py
@@ -74,6 +76,7 @@ if __name__ == '__main__':
     client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
     client.loop_start()
 
+    '''
     x = []
     y = []
     winner = 0
@@ -86,28 +89,45 @@ if __name__ == '__main__':
 
     ani = animation.FuncAnimation(fig, animate, fargs=(x,y,x_val,y_val, winner), interval=1000)
     plt.show()
+    '''
+    progress = 0
+    rounds = -1
+    x = []
+    y = []
 
     while True:
         time.sleep(1)
         if len(rpi1_values) >= 5 and len(rpi2_values) >= 5:
+        #if len(rpi1_values) == 5:
+            rounds += 1
             remove_max(rpi1_values)
             remove_max(rpi2_values)
             remove_min(rpi1_values)
             remove_min(rpi2_values)
-            print(rpi1_values)
-            print(rpi2_values)
+            #print(rpi1_values)
+            #print(rpi2_values)
+            
+            avg1 = sum(rpi1_values)/len(rpi1_values)
+            avg2 = sum(rpi2_values)/len(rpi2_values)
+            #avg2 = 5
+            
             rpi1_values = []
             rpi2_values = []
 
-            avg1 = sum(rpi1_values)/len(rpi1_values)
-            avg2 = sum(rpi2_values)/len(rpi2_values)
             if avg1 > avg2:
-                winner = 1
+                progress += 1
             elif avg2 > avg1:
-                winner = -1
+                progress -= 1
             else:
-                winner  = 0
-            print("winner:", winner)
+                progress += 0
+            y.append(progress)
+            x.append(rounds)
+        if progress == 5 or progress == -5:
+            break
+    plt.plot(x,y)
+    plt.xlabel('time')
+    plt.ylabel('progress')
+    plt.show()
 
 
 
